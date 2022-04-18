699b29bf34fe6c8ca643d0361975a0e253630a2d
Юлия Москалева <yulia@MacBook-Pro-Ulia.local>
2022-04-18 19:00:30 +0300

    Маршрутизатор

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,19 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* Страница Карамельки */
+router.get('/karamelka', function(req, res, next) {
+  res.send("<h1>Страница Карамельки</h1>");
+});
+
+/* Страница Компота */
+router.get('/kompot', function(req, res, next) {
+  res.send("<h1>Страница Компота</h1>");
+});
+
+/* Страница Коржика */
+router.get('/korthik', function(req, res, next) {
+  res.send("<h1>Страница Коржика</h1>");
+});
+
 module.exports = router;
