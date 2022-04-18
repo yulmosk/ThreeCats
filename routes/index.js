590e69eeda35d85d0f79d20b5fac84b8c90d06c2
Юлия Москалева <yulia@MacBook-Pro-Ulia.local>
2022-04-18 21:36:39 +0300

    Шаблон cat

--- routes/index.js
+++ routes/index.js
@@ -6,19 +6,32 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-/* Страница Карамельки */
+/* GET  Карамельке page. */
 router.get('/karamelka', function(req, res, next) {
-  res.send("<h1>Страница Карамельки</h1>");
+  res.render('cat', { 
+  	title: 'Карамелька',
+  	picture: "images/karamelka.jpeg",
+  	desc: "Cамый младший член семьи. Коронная фраза — «Я знаю, что надо делать!». Носит красный бант и красное платье."
+  });
 });
 
-/* Страница Компота */
+/* GET  Компот page. */
 router.get('/kompot', function(req, res, next) {
-  res.send("<h1>Страница Компота</h1>");
+  res.render('cat', { 
+  	title: 'Компот',
+  	picture: "images/kompont.png",
+  	desc: "Cтарший котёнок в семье. Ходит в школу. Очень любит грибы, читает про них энциклопедии. Любит игры на логику. Когда очень сильно огорчается, восклицает «Ну вот!» и начинает плакать. Обожает вкусно поесть. Носит зелёный костюм и шапку."
+  });
 });
 
-/* Страница Коржика */
+/* GET  Коржик page. */
 router.get('/korthik', function(req, res, next) {
-  res.send("<h1>Страница Коржика</h1>");
+  res.render('cat', { 
+  	title: 'Коржик',
+  	picture: "images/korthik.png",
+  	desc: "средний котёнок в семье. Очень любит футбол. Любит бегать, прыгать и веселиться. Иногда делает такие вещи, что из них приходится выбираться всей семье. Одет в форму моряка."
+  });
 });
+
 
 module.exports = router;
