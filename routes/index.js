6be65311e614cf6b56eb879bb9ecd4fbd73ebca4
Юлия Москалева <yulia@MacBook-Pro-Ulia.local>
2022-04-19 23:12:12 +0300

    Запись в куки

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var Cat = require("../models/cat").Cat
 /* GET home page. */
 router.get('/', function(req, res, next) {
   Cat.find({},{_id:0,title:1,nick:1},function(err,menu){
-    res.render('index', {
+    res.cookie('greeting', 'Hi!!!').render('index', {
                             title: 'Express',
                             menu: menu
                         });
