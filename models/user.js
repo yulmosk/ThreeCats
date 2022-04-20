f48bbb4111183a5efc5bc43bab97b65663382021
Юлия Москалева <yulia@MacBook-Pro-Ulia.local>
2022-04-20 22:53:37 +0300

    Создание модели User

---
+++ models/user.js
@@ -0,0 +1,37 @@
+var mongoose = require('mongoose')
+var crypto = require("crypto")
+var Schema = mongoose.Schema
+
+var userSchema = new Schema({
+    username:{
+        type: String,
+        unique: true,
+        required: true
+    },
+    hashedPassword:{
+        type: String,
+        required: true
+    },
+    salt:{
+        type: String,
+        required: true
+    },
+    created:{
+        type:Date,
+        default: Date.now
+    }
+})
+
+userSchema.virtual("password").set(function(password){
+    this._purePassword = password
+    this.salt = Math.random() + ""
+    this.hashedPassword = this.encryptPassword(password)
+}).get(function(){
+    return this._purePassword
+})
+
+userSchema.methods.encryptPassword = function(password){
+    return crypto.createHmac('sha1', this.salt).update(password).digest('hex')
+}
+
+module.exports.User = mongoose.model("User",userSchema)
