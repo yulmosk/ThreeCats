e5ab9c9bf962b92eea4185026877b6f9fe9b9599
Юлия Москалева <yulia@MacBook-Pro-Ulia.local>
2022-04-20 23:04:14 +0300

    Роутер для создания пользователей

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var User = require("./../models/User").User
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -13,6 +14,20 @@ router.get('/', function(req, res, next) {
 /* GET login/registration page. */
 router.get('/logreg', function(req, res, next) {
   res.render('logreg',{title: 'Вход'});
+});
+
+/* POST login/registration page. */
+router.post('/logreg', function(req, res, next) {
+  var username = req.body.username
+  var password = req.body.password
+  User.findOne({username:username},function(err,user){
+    if(err) return next(err)
+    if(user){
+	res.send("<h1>Пользователь найден</h1>");
+    } else {
+	res.send("<h1>Пользователь НЕ найден</h1>")
+}
+})
 });
 
 module.exports = router;
